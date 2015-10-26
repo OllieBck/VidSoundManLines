0743c95983f78d0ac62c05ae62dd0196715f12ba
OllieBck <jasonbeck@172-16-249-8.DYNAPOOL.NYU.EDU>
2015-10-26 10:57:53 -0400

    change color of lines to pixel color (RGB) of pixel

--- sketch.js
+++ sketch.js
@@ -6,6 +6,8 @@ var micInput;
 var darkPoint = [];
 var radius = 5;
 var degree = 0;
+var backRed, backGreen, backBlue, redValue, greenValue, blueValue, backAlpha, alphaValue;
+var redInst, greenInst, blueInst, alphaInst;
 
 
 function setup() {
@@ -17,12 +19,35 @@ function setup() {
   vid.size(width, height);
   vid.hide();
   img = createImage(vid.width, vid.height);
-  micInput = new p5.AudioIn();
+  micInput = new p5.AudioIn(); //sound library?
   micInput.start();
+  backRed = createSlider(0, 255, 255);
+  backRed.position(width-width/4, height/12);
+  redInst = createDiv("Control red value of background");
+  redInst.style("color", "#ff0000")
+  redInst.position(width-width/4, height/12+25);
+  backGreen = createSlider(0, 255, 255);
+  greenInst = createDiv("Control green value of background");
+  greenInst.style("color", "#00e500");
+  greenInst.position(width-width/4, height/12+75);
+  backGreen.position(width-width/4, height/12+50);
+  backBlue = createSlider(0, 255, 255);
+  backBlue.position(width-width/4, height/12+100);
+  blueInst = createDiv("Control blue value of background");
+  blueInst.style("color", "#0000ff");
+  blueInst.position(width-width/4, height/12+125);
+  backAlpha = createSlider(0, 255, 255);
+  backAlpha.position(width-width/4, height/12+150);
+  alphaInst = createDiv("Control opacity of background");
+  alphaInst.position(width-width/4, height/12+175);
 }
 
 function draw() {
-background(0);
+redValue = backRed.value();
+greenValue = backGreen.value();
+blueValue = backBlue.value();
+alphaValue = backAlpha.value();
+background(redValue, greenValue, blueValue, alphaValue);
 //load the pixels from the camera image
   vid.loadPixels();
 // go line by line of that image to get the pixel properties, but skip some to limit results
@@ -34,13 +59,13 @@ background(0);
       var g = pixelArray[1];
       var b = pixelArray[2];
       var vol = micInput.getLevel();
-//find the darkest pixels and draw an ellipse there
-      if (r+g+b-255 > 0){ 
+//find the darker pixels and draw spinning lines there there
+      if (r+g+b-255 < 0){ 
         radius = vol*100;
-        stroke(255, 255, 255);
+        stroke([r, g, b, 255]);
         strokeWeight(1);
-        line(x+width/4, y+height/12, x+width/4+radius*cos(degree), y+height/12+radius*sin(degree));
-        degree = degree +10;
+        line(x+width/6, y+height/12, x+width/6+radius*cos(degree), y+height/12+radius*sin(degree));
+        degree = degree +1;
       }
     }
   }
